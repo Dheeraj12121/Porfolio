9a38999f5cf76f4359863532c566976f48f3e38d
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2023-01-12 13:38:23 +0530

    margin

--- components/Hero.js
+++ components/Hero.js
@@ -9,8 +9,8 @@ const Hero = () => {
         <p className="leading-9">
           {/* I'm a software engineer based in New York, NY */}
         </p>
-        <a href="https://drive.google.com/file/d/1xTsxILUY-nIvXT8TqE5IDgmei14vj-On/view" download>
-        <button className="pt-4 text-purple-800 bg-purple-300 px-2 rounded-full w-[90px]">
+        <a className="mt-2" href="https://drive.google.com/file/d/1xTsxILUY-nIvXT8TqE5IDgmei14vj-On/view" download>
+        <button className="  text-purple-800 bg-purple-300 px-2 rounded-full w-[90px]">
           Resume
         </button>
         </a>
