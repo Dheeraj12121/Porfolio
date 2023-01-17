3ea332049aab808b541794253cfb9838cc1fc77d
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2023-01-17 15:49:50 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="grid grid-cols-1  gap-y-10 px-32  py-10 bg-gray-100 text-gray-600 ">
       <div className="space-y-4  text-xs text-gray-800">
-        <h5 className="font-bold  text-center"> dkreal50@gamil.com</h5>
+        <h5 className="font-bold  text-center"> dkreal50@gmail.com</h5>
         
       </div>
       
